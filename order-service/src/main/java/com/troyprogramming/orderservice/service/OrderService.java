e8e3706f3a2e36fe1bb4a491491a9c6498415f39
abako <abako220@gmail.com>
2023-04-05 20:38:48 +0100

    added dtos for response and also added getAllOrder method

--- order-service/src/main/java/com/troyprogramming/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/troyprogramming/orderservice/service/OrderService.java
@@ -1,6 +1,8 @@
 package com.troyprogramming.orderservice.service;
 
 import com.troyprogramming.orderservice.dto.OrderRequest;
+import com.troyprogramming.orderservice.dto.OrderResponse;
+import com.troyprogramming.orderservice.dto.OrderResponseList;
 import com.troyprogramming.orderservice.dto.OrderlineItemsDto;
 import com.troyprogramming.orderservice.model.Order;
 import com.troyprogramming.orderservice.model.OrderLineItems;
@@ -8,8 +10,8 @@ import com.troyprogramming.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -36,4 +38,49 @@ public class OrderService {
                 .build();
         return orderLineItems;
     }
+
+    public OrderLineItems mapToResponseDto(OrderResponseList orderResponseList) {
+        OrderLineItems orderLineItems = OrderLineItems.builder()
+                .id(orderResponseList.getId())
+                .skuCode(orderResponseList.getSkuCode())
+                .price(orderResponseList.getPrice())
+                .quantity(orderResponseList.getQuantity())
+                .build();
+        return orderLineItems;
+    }
+
+    public List<OrderResponse> getAllOrder() {
+        List<OrderResponse> orderResponseList = new ArrayList<>();
+         orderRepository.findAll()
+                .forEach(order -> {
+                    OrderResponse orderResponse = OrderResponse.builder()
+                            .orderNumber(order.getOrderNumber())
+                            .orderResponseLists(
+                                    this.maptoResponseDto(order.getOrderLineItemsList())
+
+                            ).build();
+                    orderResponseList.add(orderResponse);
+                });
+         return  orderResponseList;
+
+    }
+
+    public List<OrderResponseList> maptoResponseDto(List<OrderLineItems> lineItems) {
+        List<OrderResponseList> orderResponseLists = new ArrayList<>();
+        for(OrderLineItems orderLineItems : lineItems) {
+            orderResponseLists.add(maptoResponseDto1(orderLineItems));
+        }
+        return orderResponseLists;
+    }
+
+    public OrderResponseList maptoResponseDto1(OrderLineItems lineItems) {
+        return OrderResponseList.builder()
+                .id(lineItems.getId())
+                .price(lineItems.getPrice())
+                .quantity(lineItems.getQuantity())
+                .skuCode(lineItems.getSkuCode())
+                .build();
+
+    }
+
 }
