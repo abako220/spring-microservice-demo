d73ab749706f64169fcfee6d8a29768cc9cd3c6f
abako <abako220@gmail.com>
2023-04-15 10:56:57 +0100

    added auhentication from browser and also added username and password to eureka discovery endpoint

--- microservice-parent/discovery-server/src/main/java/com/troyprogramming/discoveryservice/config/SecurityConfig.java
+++ microservice-parent/discovery-server/src/main/java/com/troyprogramming/discoveryservice/config/SecurityConfig.java
@@ -1,17 +1,63 @@
 package com.troyprogramming.discoveryservice.config;
 
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cglib.proxy.NoOp;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder.inMemoryAuthentication()
-                .withUser("eureka").password("password")
-                .authorities("USER");
+public class SecurityConfig extends SecurityConfigurerAdapter {
+
+    @Value("${eureka.username}")
+    private String eurekaUsername;
+
+    @Value("${eureka.password}")
+    private String eurekaPassword;
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        //User Role
+        UserDetails theUser = User.withUsername(eurekaUsername)
+                .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder()::encode)
+                .password(eurekaPassword).roles("USER").authorities("USER").build();
+
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+
+        userDetailsManager.createUser(theUser);
+
+        return userDetailsManager;
+
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests().anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+            //http.headers().frameOptions().sameOrigin();
+
+        return http.build();
+    }
+
 }
